c71ca854d956ec74527c4b8f06b99f63ea91cb70
Chase Donovan <chase.donovan@aol.com>
2022-09-02 14:09:28 -0700

    changed getform endpoint

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -76,7 +76,7 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                // action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                action='https://getform.io/f/3885b420-35e2-45d7-95bc-3cefa13ab4b2'
                 method='POST'
                 encType='multipart/form-data'
               >
